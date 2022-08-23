628c97e0a49f1c904eaafa43d68c9665905d6f7c
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-08-23 22:28:03 +0200

    minimal distance in splits and print representation

--- interfaces/splitter.py
+++ interfaces/splitter.py
@@ -10,7 +10,17 @@ class ISplit(ABC):
     @abstractmethod
     def splits(self) -> Iterable[Iterable[int]]:
         pass
+    @abstractmethod
+    def minimalDistanceBetweenSplits(self) -> float:
+        pass
 
+    def __repr__(self):
+        splits = self.splits()
+        s = sum(map(len, splits))
+        return f"Split<distribution = [{', '.join(map(lambda x: '{0:.3f}'.format(len(x) / s), splits))}], minDistance = {'{0:.3g}'.format(self.minimalDistanceBetweenSplits())}>"
+    
+    def __str__(self):
+        return self.__repr__()
 """
 This class is an abstract class for splitting assets into chunks by IDs and distances from the distance matrix.
 """
